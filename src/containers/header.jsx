47150702385cab7ae7d9749e504ff6854e8bb492
CyberGA <etomu.joshua@gmail.com>
2022-01-07 03:59:04 +0100

    background changer

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/header/index";
 // import * as ROUTES from "../constants/routes";
 import { HOME, SIGN_IN } from "../constants/routes"; //destructured home and signin
 
 function showSigninBtn() {
-  if(document.baseURI.includes("signin") || document.baseURI.includes("signup")) return
+  if (
+    document.baseURI.includes("signin") ||
+    document.baseURI.includes("signup")
+  )
+    return;
 
-  return (<Header.SigninBtn to={SIGN_IN}>Sign In</Header.SigninBtn>)
+  return <Header.SigninBtn to={SIGN_IN}>Sign In</Header.SigninBtn>;
 }
 
 export default function HeaderContainer({ children }) {
+  const [height, setHeight] = useState(window.innerWidth);
+  const [show, setShow] = useState(true);
+
+  window.onresize = () => setHeight(window.innerWidth);
+
+  useEffect(() => {
+    if (
+      (document.baseURI.includes("signin") ||
+        document.baseURI.includes("signup")) &&
+      height <= 739
+    ) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  }, [height]);
+
   return (
     // console.log(document.baseURI)
-    <Header>
+    <Header bg={show}>
       <Header.Frame>
         <Header.Logo
           to={HOME}
