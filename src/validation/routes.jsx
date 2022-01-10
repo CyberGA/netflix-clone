0a49e252060e4534db518bf4ad414d87843669ec
CyberGA <etomu.joshua@gmail.com>
2022-01-10 23:45:23 +0100

    made correction to react-router-dom import

--- src/validation/routes.jsx
+++ src/validation/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router";
+import { Route, Redirect } from "react-router-dom";
 
 export function IsUserValidated({ user, loggedInPath, children, ...props }) {
   return (
