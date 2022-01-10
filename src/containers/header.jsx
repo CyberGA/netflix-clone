607b1b719b266e4f6a38bf3857b3164493160c8f
CyberGA <etomu.joshua@gmail.com>
2022-01-10 23:41:28 +0100

    remove code manipulating dom directly and add useLocation to get the pathname

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/header/index";
-// import * as ROUTES from "../constants/routes";
+import * as ROUTES from "../constants/routes";
+import { useLocation } from "react-router-dom";
 import { HOME, SIGN_IN } from "../constants/routes"; //destructured home and signin
-
-function showSigninBtn() {
-  if (
-    document.baseURI.includes("signin") ||
-    document.baseURI.includes("signup")
-  )
-    return;
-
-  return <Header.SigninBtn to={SIGN_IN}>Sign In</Header.SigninBtn>;
-}
 
 export default function HeaderContainer({ children }) {
   const [height, setHeight] = useState(window.innerWidth);
   const [show, setShow] = useState(true);
+  const { pathname } = useLocation();
 
   window.onresize = () => setHeight(window.innerWidth);
 
   useEffect(() => {
     if (
-      (document.baseURI.includes("signin") ||
-        document.baseURI.includes("signup")) &&
+      (pathname === ROUTES.SIGN_IN || pathname === ROUTES.SIGN_UP) &&
       height <= 739
     ) {
       setShow(false);
     } else {
       setShow(true);
     }
-  }, [height]);
+  }, [height, pathname]);
 
   return (
     // console.log(document.baseURI)
@@ -45,4 +36,10 @@ export default function HeaderContainer({ children }) {
       {children}
     </Header>
   );
+
+  function showSigninBtn() {
+    if (pathname === ROUTES.SIGN_IN || pathname === ROUTES.SIGN_UP) return;
+
+    return <Header.SigninBtn to={SIGN_IN}>Sign In</Header.SigninBtn>;
+  }
 }
