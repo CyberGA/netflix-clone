26872d45b9e0c4d72b4e3a638bb51f3b6d7b0dfb
CyberGA <etomu.joshua@gmail.com>
2022-01-12 22:43:07 +0100

    update to latest firebase recommendation

--- src/hooks/use-auth-listener.jsx
+++ src/hooks/use-auth-listener.jsx
@@ -1,19 +1,24 @@
 import { useState, useEffect, useContext } from "react";
 import { FirebaseContext } from "../context/firebase";
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 export default function useAuthListener() {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
 
-  const { firebase } = useContext(FirebaseContext);
+  const { firebaseApp } = useContext(FirebaseContext);
 
   useEffect(() => {
-    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+    const auth = getAuth(firebaseApp);
+    const listener = onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
         localStorage.setItem("authUser", JSON.stringify(authUser.providerData));
         setUser(authUser);
+        console.log("there is user");
       } else {
+        console.log("there is no user");
         localStorage.removeItem("authUser");
         setUser(null);
       }
