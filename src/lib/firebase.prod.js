f0e40a955b59fe02a4066e39e6890dd082d02845
CyberGA <etomu.joshua@gmail.com>
2022-01-12 22:39:29 +0100

    updated firebase code for initializing firebase to v9 recommend

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
 import { FIREBASE_CONFIG } from "./secret";
 // import { seedDatabase } from "../seed";
 
 // config
 const config = FIREBASE_CONFIG;
 
-const firebase = initializeApp(config);
+const firebaseApp = initializeApp(config);
 
 //storing the data from the seed
 // seedDatabase(firebase);
 
-export { firebase };
+export { firebaseApp };
