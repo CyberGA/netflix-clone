9d3b69a4674118ad6ca073822305bb6727d84cb1
CyberGA <etomu.joshua@gmail.com>
2022-01-15 05:44:41 +0100

    firebase signin

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -5,32 +5,16 @@ import HeaderContainer from "./../containers/header";
 import FooterContainer from "./../containers/footer";
 import Form from "../components/form";
 import { HomeBgGradient } from "../components/header/styles/header";
-import * as ROUTES from "../lib/routes";
+import { signinAuth } from "./../service/auth";
 
-export default function Signin({
-  loginForm: { onChange, form, loginFormNotValid, resetForm },
-}) {
+export default function Signin({ loginForm: { onChange, form, loginFormNotValid, resetForm } }) {
   const history = useHistory();
-
-  const { firebase } = useContext(FirebaseContext);
+  const { firebaseApp } = useContext(FirebaseContext);
   const [error, setError] = useState("");
 
-  const handleSignin = (e) => {
+  const handleSignin = async (e) => {
     e.preventDefault();
-
-    // firebase will confirm here
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(form.email, form.password)
-      .then(() => {
-        //? redirect to the browse page
-        history.push(ROUTES.BROWSE);
-      })
-      .catch((err) => {
-        //? empty the form
-        resetForm()
-        setError(err.message);
-      });
+    signinAuth(form, firebaseApp, history, resetForm, setError);
   };
 
   return (
@@ -64,19 +48,33 @@ export default function Signin({
               Sign In
             </Form.Submit>
 
+            <Form.Pane bottom={`54px`} space={`space-between`}>
+              <Form.SmallText>
+                <input type="checkbox" /> {"  "}
+                Remember me
+              </Form.SmallText>
+              <Form.Link to="#" useSmall={true}>
+                Need help?
+              </Form.Link>
+            </Form.Pane>
+
+            <Form.LinkText>
+              <img src="../images/misc/FB-f-Logo__blue_57.png" alt="facebook" width={20} height={20} />
+              <Form.Link to="#">Login with Facebook</Form.Link>
+            </Form.LinkText>
+
             <Form.Text>
               New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
             </Form.Text>
 
             <Form.SmallText>
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot. <Form.Link to="#">Learn more.</Form.Link>
+              This page is protected by Google reCAPTCHA to ensure you're not a bot. <Form.Link to="#">Learn more.</Form.Link>
             </Form.SmallText>
           </Form.Base>
         </Form>
       </HeaderContainer>
 
-      <FooterContainer></FooterContainer>
+      <FooterContainer page={"auth"}></FooterContainer>
     </>
   );
 }
