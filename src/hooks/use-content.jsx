0f49444a1390fb3b751fa7cd531e1b9d234aa4bb
CyberGA <etomu.joshua@gmail.com>
2022-01-16 22:45:05 +0100

    bug fix in getting data

--- src/hooks/use-content.jsx
+++ src/hooks/use-content.jsx
@@ -5,17 +5,18 @@ export default function useContent(target, name) {
   const [content, setContent] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const request = await fetch(`${requests.baseURL}${target}`)
+    const fetchData = async () =>
+      await fetch(`${requests.baseURL}${target}`)
         .then((res) => res.json())
+        .then(({ results }) => {
+          setContent((content) => results);
+        })
         .catch((err) => console.log(err.message));
 
-      setContent((content) => request.results);
-
-      return request;
-    }
     fetchData();
-  }, [target]);
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return { [name]: content };
 }
