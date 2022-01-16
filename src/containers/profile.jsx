65086ffdb930f373ffd8f8882265d836ab8a7f5c
CyberGA <etomu.joshua@gmail.com>
2022-01-16 22:42:58 +0100

    select profile container component

---
+++ src/containers/profile.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "../components/header";
+import { BROWSE } from "../lib/routes";
+import Profiles from "../components/profiles";
+
+const SelectProfileContainer = ({ user, setProfile }) => {
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo to={BROWSE} src="/images/misc/Netflix-logo.svg" alt="Netflix logo" />
+        </Header.Frame>
+      </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    </>
+  );
+};
+
+export default SelectProfileContainer;
