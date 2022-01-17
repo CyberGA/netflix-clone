9fda5ea4cd305fff3461e3bf4dd06843816d0fdb
CyberGA <etomu.joshua@gmail.com>
2022-01-17 23:47:04 +0100

    banner, banner image fetch,  random banner image

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -2,27 +2,39 @@ import React, { useState, useEffect } from "react";
 import SelectProfileContainer from "./profile";
 import { getAuth } from "firebase/auth";
 import Loading from "../components/loading/index";
+import useContent from "../hooks/use-content";
+import requests from "../lib/request";
+import Header from "../components/header/index";
 
 const BrowseContainer = () => {
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
-  const user = getAuth().currentUser || {};
+  const [banner, setBanner] = useState("");
+  const authUser = getAuth().currentUser || {};
+  const { Action } = useContent(`${requests.fetchActionMovies}`, "Action");
 
   useEffect(() => {
     console.log("profile", profile);
+    const newBanner = Action[Math.ceil(Math.random() * Action.length - 1)];
+    console.log(newBanner);
+    setBanner((banner) => newBanner);
     setTimeout(() => {
       setLoading((loading) => false);
     }, 3000);
-  }, [profile]);
+  }, [profile, Action]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : (
-      <Loading.ReleaseBody />
-    )
+    <>
+      {loading ? <Loading src={authUser.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src={`https://image.tmdb.org/t/p/original/${banner?.backdrop_path}`} bg={true}>
+        <Header.Feature>
+          <Header.Text>{banner?.overview}</Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
-    <SelectProfileContainer user={user} setProfile={setProfile} />
+    <SelectProfileContainer user={authUser} setProfile={setProfile} />
   );
 };
 
