0732ba6bfe2307028e85cc7b4d41f49f90c3bd37
CyberGA <etomu.joshua@gmail.com>
2022-01-23 23:46:19 +0100

    selection for movies

---
+++ src/lib/selection-filter.js
@@ -0,0 +1,48 @@
+export default function SelectionFIlter({ Action, Anime, Trending, Comedy, NetflixOriginals, TopRated, Crime, Horror, Romance, Documentaries }) {
+  return {
+    series: [
+      {
+        title: "Netflix Originals",
+        data: NetflixOriginals,
+      },
+      {
+        title: "Comedies",
+        data: Comedy,
+      },
+      {
+        title: "Documentaries",
+        data: Documentaries,
+      },
+      {
+        title: "Crime",
+        data: Crime,
+      },
+      {
+        title: "Action",
+        data: Action,
+      },
+    ],
+    films: [
+      {
+        title: "Top Rated",
+        data: TopRated,
+      },
+      {
+        title: "Trending",
+        data: Trending,
+      },
+      {
+        title: "Horror",
+        data: Horror,
+      },
+      {
+        title: "Anime",
+        data: Anime,
+      },
+      {
+        title: "Romance",
+        data: Romance,
+      },
+    ],
+  };
+}
