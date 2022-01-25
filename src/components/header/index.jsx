ff977b452d83f261d99522fa668bdefa57ff2e8b
CyberGA <etomu.joshua@gmail.com>
2022-01-25 23:45:38 +0100

    user pics, dropdown and signout added to header on browse page

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link as HomeLinked } from "react-router-dom";
-import { Background, Logo, Container, SigninBtn, Feature, Text, FeautureTitle } from "./styles/header";
+import { Background, Logo, Container, SigninBtn, Picture, Dropdown, Profile, Feature, Text, FeautureTitle, Links, Group } from "./styles/header";
 export default function Header({ bg, children, ...props }) {
   return bg ? <Background {...props}>{children}</Background> : children;
 }
@@ -8,8 +8,17 @@ export default function Header({ bg, children, ...props }) {
 Header.Feature = function HeaderFeature({ children, ...props }) {
   return <Feature {...props}>{children}</Feature>;
 };
+
 Header.Text = function HeaderText({ children, ...props }) {
   return <Text {...props}>{children}</Text>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...props }) {
+  return <Dropdown {...props}>{children}</Dropdown>;
+};
+
+Header.Links = function HeaderLinks({ children, ...props }) {
+  return <Links {...props}>{children}</Links>;
 };
 Header.FeautureTitle = function HeaderFeautureTitle({ children, ...props }) {
   return <FeautureTitle {...props}>{children}</FeautureTitle>;
@@ -17,6 +26,18 @@ Header.FeautureTitle = function HeaderFeautureTitle({ children, ...props }) {
 
 Header.Frame = function HeaderFrame({ children, ...props }) {
   return <Container {...props}>{children}</Container>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...props }) {
+  return <Picture {...props} src={`/images/user/${src}.png`} />;
+};
+
+Header.Profile = function HeaderProfile({ children, ...props }) {
+  return <Profile {...props}>{children}</Profile>;
+};
+
+Header.Group = function HeaderGroup({ children, ...props }) {
+  return <Group {...props}>{children}</Group>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...props }) {
