7c6e8fb6f50e669b2a7945d677879bede485f3f3
CyberGA <etomu.joshua@gmail.com>
2022-01-27 08:53:19 +0100

    to display films in a row

---
+++ src/components/card/index.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useContext } from "react";
+import { Container, Group, Title, Image, SubTitle, Text, Meta, Item } from "./styles/card";
+
+export const FeatureContext = createContext();
+
+const Card = ({ children, ...props }) => {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}>
+      <Container {...props}>{children}</Container>
+    </FeatureContext.Provider>
+  );
+};
+
+Card.Group = function CardGroup({ children, ...props }) {
+  return <Group {...props}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...props }) {
+  return <Title {...props}>{children}</Title>;
+};
+
+Card.SubTitle = function CardSubTitle({ children, ...props }) {
+  return <SubTitle {...props}>{children}</SubTitle>;
+};
+
+Card.Text = function CardText({ children, ...props }) {
+  return <Text {...props}>{children}</Text>;
+};
+
+Card.Meta = function CardMeta({ children, ...props }) {
+  return <Meta {...props}>{children}</Meta>;
+};
+
+Card.Item = function CardItem({ item, children, ...props }) {
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+  return (
+    <Item
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+      {...props}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({...props }) {
+    return <Image {...props} />;
+  };
+
+export default Card;
