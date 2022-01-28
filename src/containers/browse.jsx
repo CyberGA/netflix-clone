68fb95450540fd8a81acbfa3a9d39f5191533238
CyberGA <etomu.joshua@gmail.com>
2022-01-28 23:54:34 +0100

    states to manage single movie, row of movies and getting the movies

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -8,12 +8,16 @@ import Header from "../components/header/index";
 import { HOME } from "./../lib/routes";
 import getInitials from "../lib/get-initials";
 import shortened from "../lib/shorten-text";
+import Card from "../components/card";
 
 const BrowseContainer = ({ slides }) => {
+  const [category, setcategory] = useState("series");
   const [searchTerm, setSearchTerm] = useState("");
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
   const [banner, setBanner] = useState("");
+  const [slideRows, setSlideRows] = useState([]);
+
   const authUser = getAuth().currentUser || {};
   const { Trending } = useContent(`${requests.fetchTrending}`, "Trending");
 
@@ -25,6 +29,10 @@ const BrowseContainer = ({ slides }) => {
       setLoading((loading) => false);
     }, 5000);
   }, [profile, Trending]);
+
+  useEffect(() => {
+    setSlideRows((prevSlide) => slides[category]);
+  }, [slides, category]);
 
   function signOut() {
     return getAuth().signOut;
@@ -38,11 +46,15 @@ const BrowseContainer = ({ slides }) => {
         <Header.Frame>
           <Header.Group>
             <Header.Logo to={HOME} src="/images/misc/Netflix-logo.svg" alt="Netflix logo" />
-            <Header.Links>Series</Header.Links>
-            <Header.Links>Films</Header.Links>
+            <Header.Links active={category === "series" ? true : false} onClick={() => setcategory((prevCategory) => "series")}>
+              Series
+            </Header.Links>
+            <Header.Links active={category === "films" ? true : false} onClick={() => setcategory((prevCategory) => "films")}>
+              Films
+            </Header.Links>
           </Header.Group>
           <Header.Group>
-            <Header.Search searchTerm={searchTerm}  setSearchTerm={setSearchTerm} />
+            <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             <Header.Profile>
               <Header.Picture src={authUser.photoURL} />
               <Header.Dropdown>
@@ -61,6 +73,31 @@ const BrowseContainer = ({ slides }) => {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.id} item={item}>
+                  <Card.Image src={`${requests.img_url}${item?.poster_path}` ?? `${requests.img_url}${item?.backdrop_path}`} />
+                  <Card.Meta>
+                    <Card.SubTitle>{item?.title ?? item.original_title ?? item?.original_name}</Card.SubTitle>
+                    <Card.Text>{item?.overview}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            {/* <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src="" />
+              </Player>
+            </Card.Feature> */}
+          </Card>
+        ))}
+      </Card.Group>
     </>
   ) : (
     <SelectProfileContainer user={authUser} setProfile={setProfile} />
