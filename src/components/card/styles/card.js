4f89a2cd13979c806c3a123ba7e85829cee6670f
CyberGA <etomu.joshua@gmail.com>
2022-01-30 23:45:47 +0100

    styling card for movies

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -43,20 +43,52 @@ export const Text = styled.p``;
 
 export const Feature = styled.div``;
 
-export const FeatureTitle = styled(Title)``;
+export const FeatureTitle = styled(Title)`
+  margin-left: 0;
+`;
 
 export const FeatureText = styled.p``;
 
-export const FeatureClose = styled.button``;
+export const FeatureClose = styled.button`
+  color: #fff;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  background: transparent;
+  border: 0;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+  }
+`;
 
 export const Maturity = styled.div``;
 
 export const Content = styled.div``;
 
-export const Meta = styled.div``;
+export const Meta = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  background-color: #00000008;
+`;
 
-export const Entities = styled.div``;
+export const Entities = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 export const Item = styled.div``;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  border: 0;
+  width: 100%;
+  max-width: 305px;
+  cursor: pointer;
+  height: auto;
+  padding: 0;
+  margin: 0;
+`;
