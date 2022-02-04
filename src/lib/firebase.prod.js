85bb84f7caebd2a04526699996b7320f998447b9
CyberGA <etomu.joshua@gmail.com>
2022-02-04 22:04:47 +0100

    functionality description

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { FIREBASE_CONFIG } from "./secret";
 // import { seedDatabase } from "../seed";
 
-//? config
+//& config
 const config = FIREBASE_CONFIG;
 
 const firebaseApp = initializeApp(config);
