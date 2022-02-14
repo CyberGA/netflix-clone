169c7b1822108bcffc5db4210df1e419b7dd0bbd
CyberGA <etomu.joshua@gmail.com>
2022-02-14 22:33:58 +0100

    404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        background: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#000",
+        fontSize: "32px",
+        height: "100vh",
+      }}
+    >
+      URL NOT FOUND
+    </div>
+  );
+};
+
+export default NotFound;
